31c7742036551f7b06b6c65c4e153f99d27f76a9
Yuichi NAGAKURA <yuichi.nagakura@asia-quest.jp>
2022-06-14 16:36:07 +0900

    パス変更

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/logfile-parser/dist/',
+  base: '/logfile-parser',
   build: {
     chunkSizeWarningLimit: 1500,
     minify: true,
